7bff777a1a3811b20158baaf94131be615e6dc69
Evelyn Asmat <evelyn.asmat@gmail.com>
2024-01-20 10:39:59 -0500

    [ADD] Sort cards

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        AQUI VOY A ORDENAR LAS CARDS
       </div>
       <h1>Vite + React</h1>
       <div className="card">
